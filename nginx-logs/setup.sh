5bbe6a25b6330b82f031a30fe6ea17a60ec1073e
Leander Ziehm <leander@leanderziehm.com>
2026-08-09 01:02:05 +0200

    add container

--- nginx-logs/setup.sh
+++ nginx-logs/setup.sh
@@ -1,3 +1,4 @@
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+podman build -t nginx-logs .
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip install -r requirements.txt
